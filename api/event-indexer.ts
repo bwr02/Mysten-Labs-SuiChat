0fc15986902adb669f482f3a165b475b504e2206
Ashton Wong <ashton@ucsb.edu>
2024-11-05 18:04:25 -0800

    database initial commit

--- api/event-indexer.ts
+++ api/event-indexer.ts
@@ -1,10 +1,20 @@
 import { EventId, SuiClient, SuiEvent, SuiEventFilter } from '@mysten/sui/client';
 import { CONFIG } from './config';
+import { Prisma } from '@prisma/client';
+import { prisma } from './db';
 import { getClient } from './sui-utils';
 
 type SuiEventsCursor = EventId | null | undefined;
 
-interface MessageCreatedEvent {
+type MessageCreatedEvent {
+    sender: string;
+    recipient: string;
+    message_id: string;
+    content: Uint8Array;
+    timestamp: number;
+}
+
+type MessageCreateInput = {
     sender: string;
     recipient: string;
     content: Uint8Array;
@@ -14,14 +24,46 @@ interface MessageCreatedEvent {
 type EventTracker = {
     type: string;
     filter: SuiEventFilter;
-    callback: (events: SuiEvent[]) => void;
+    callback: (events: SuiEvent[], type: string) => any;
 };
 
-const handleMessageCreated = (events: SuiEvent[]) => {
+const handleMessageCreated = async (events: SuiEvent[], type: string) => {
     events.forEach(event => {
         const { sender, recipient, content, timestamp } = event.parsedJson as MessageCreatedEvent;
         console.log(`New message from ${sender} to ${recipient}: ${new TextDecoder().decode(content)} at ${timestamp}`);
     });
+
+    const updates: Record<string, MessageCreateInput> = {};
+
+	for (const event of events) {
+		if (!event.type.startsWith(type)) throw new Error('Invalid event module origin');
+		// const data = event.parsedJson as MessageCreatedEvent;
+
+		// if (!Object.hasOwn(updates, data.escrow_id)) {
+		// 	updates[data.escrow_id] = {
+		// 		objectId: data.escrow_id,
+		// 	};
+		// }
+
+		const creationData = event.parsedJson as MessageCreatedEvent;
+
+		// Handle creation event
+		updates[creationData.message_id].sender = creationData.sender;
+		updates[creationData.message_id].recipient = creationData.recipient;
+		updates[creationData.message_id].content = creationData.content;
+		updates[creationData.message_id].timestamp = creationData.timestamp;
+	}
+
+    const promises = Object.values(updates).map((update) =>
+		prisma.escrow.upsert({
+			where: {
+				objectId: update.objectId,
+			},
+			create: update,
+			update,
+		}),
+	);
+	await Promise.all(promises);
 };
 
 const EVENTS_TO_TRACK: EventTracker[] = [
