ca7f4bdf28894d49b2683f23a5ee08ce27ff903c
bonniehuynh <bonniehuynh@ucsb.edu>
2024-11-14 18:19:02 -0800

    bh - added clickable address book icon to sidebar and changed color of send bubble to be more vibrant color

--- frontend/src/components/ChatSidebar.tsx
+++ frontend/src/components/ChatSidebar.tsx
@@ -10,10 +10,35 @@ export function ChatSidebar() {
     { name: "Jane", message: "Have you done 171 HW?", time: "36 min" },
     { name: "John", message: "What time is midterm?", time: "45 min" },
   ];
+  
+  // click on address book
+  const handleBookClick = () => {
+    console.log("Address Book clicked!");
+  };
 
   return (
     <div className="chat-sidebar">
-      <h1 className="chat-title">SuiChat</h1>
+      <h1 className="chat-title">
+        SuiChat
+        <button className="book-icon" onClick={handleBookClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            fill="currentColor"
+          >
+            <path 
+              fillRule="evenodd"
+              d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+
+
+      </h1>
+      
       <h2 className="conversations-heading">Conversations</h2>
       <div className="conversations-list">
         {conversations.map((conv, index) => (
