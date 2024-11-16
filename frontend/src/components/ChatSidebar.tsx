c77db6a24cc5385b458559e11c3264eaba3b7613
bonniehuynh <bonniehuynh@ucsb.edu>
2024-11-15 23:01:15 -0800

    bh - resolved errors and added address book input field popup

--- frontend/src/components/ChatSidebar.tsx
+++ frontend/src/components/ChatSidebar.tsx
@@ -1,6 +1,12 @@
 import "../styles/ChatSidebar.css";
+import React, { useState } from "react";
 // component to display all the chat previews on the left handside
-export function ChatSidebar() {
+interface ChatSidebarProps {
+  className?: string;
+}
+export const ChatSidebar: React.FC<ChatSidebarProps> = ({ className }) => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchText, setSearchText] = useState("");
   const conversations = [
     { name: "Ben", message: "Just finished our prd doc!", time: "10 min" },
     { name: "Ashton", message: "LGTM", time: "14 min" },
@@ -10,33 +16,43 @@ export function ChatSidebar() {
     { name: "Jane", message: "Have you done 171 HW?", time: "36 min" },
     { name: "John", message: "What time is midterm?", time: "45 min" },
   ];
-  
-  // click on address book
-  const handleBookClick = () => {
-    console.log("Address Book clicked!");
+
+  // Toggle address book search input visibility
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
   };
 
   return (
     <div className="chat-sidebar">
       <h1 className="chat-title">
         SuiChat
-        <button className="book-icon" onClick={handleBookClick}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            fill="currentColor"
-          >
-            <path 
-              fillRule="evenodd"
-              d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z"
-              clipRule="evenodd"
+        <svg
+          className="book-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          fill="currentColor"
+          onClick={toggleSearch}
+        >
+          <path
+            fillRule="evenodd"
+            d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        {/* Address book search input field */}
+        {isSearchOpen && (
+          <div className="search-container">
+            <input
+              type="text"
+              className="book-input"
+              placeholder=" "
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
             />
-          </svg>
-        </button>
-
-
+          </div>
+        )}
       </h1>
       
       <h2 className="conversations-heading">Conversations</h2>
