5910c5e43df571beabd1cc1590932d97bd0c99f6
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-11-17 16:47:05 -0800

    started converting app to tailwind and removing css files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,29 +6,22 @@ import './styles/App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      {/* flex with Sidebar and Chat Panel */}
-        <Flex
-          style={{
-            background: "#F3E8FF",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <ChatSidebar className="chat-sidebar" />
-          <ChatPanel className="chat-panel" />
-        </Flex>
-      <Box
-        style={{
-          position: "absolute",
-          top: "16px",
-          right: "16px",
-        }}
-      >
-          <ConnectButton />
-        </Box>
-    </div>
+      <div className="flex flex-col h-screen bg-purple-100">
+          {/* flex with Sidebar and Chat Panel */}
+          <Flex className="h-full flex-row" style={{ background: "#F3E8FF" }}>
+              <ChatSidebar className="w-1/4 p-4 border-r bg-purple-100" />
+              <ChatPanel className="w-3/4 flex-1 bg-white" />
+          </Flex>
+          <Box
+              style={{
+                  position: "absolute",
+                  top: "16px",
+                  right: "16px",
+              }}
+          >
+              <ConnectButton/>
+          </Box>
+      </div>
   );
 }
 
