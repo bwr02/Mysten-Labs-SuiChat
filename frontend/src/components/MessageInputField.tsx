8359436a7c47953f910e5296304272bbebc88af2
Chloe Ta <cqta@ucsb.edu>
2024-11-22 15:53:06 -0800

    removed unused import

--- frontend/src/components/MessageInputField.tsx
+++ frontend/src/components/MessageInputField.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useSuiWallet } from '../hooks/useSuiWallet';
 import MessageInputBubble from './MessageInputBubble';
-import { useAccountBalance } from '@suiet/wallet-kit';
 
 interface MessageInputFieldProps {
   recipientAddress: string | null;
