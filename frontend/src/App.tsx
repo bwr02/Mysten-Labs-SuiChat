bd8b055204052d44487c32625e8ea18faccea2e1
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2025-02-10 18:24:01 -0800

    fixed first time loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import Layout from "./layout";
 import HomePage from "./pages/HomePage";
 import Contacts from "./pages/Contacts";
 import { useSuiWallet } from "@/hooks/useSuiWallet";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import {ConnectButton} from "@suiet/wallet-kit";
+import {Box} from "@radix-ui/themes";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
     const { address } = useSuiWallet(); // Pull address from Suiet to give to backend
+    const [activeAddressLoaded, setActiveAddressLoaded] = useState(false);
     // When the wallet address becomes available or changes, send it to the backend.
     useEffect(() => {
         if (address) {
@@ -19,11 +22,37 @@ function App() {
                 },
                 body: JSON.stringify({ activeAddress: address }),
             })
-                .then(response => response.json())
-                .then(data => console.log('Active address set:', data))
-                .catch(err => console.error('Error setting active address:', err));
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error("Failed to set active address");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    if (data.success) {
+                        setActiveAddressLoaded(true);
+                    }
+                })
+                .catch((err) => console.error("Error setting active address:", err));
         }
     }, [address]);
+
+    // Do not load full SuiChat until active address is set and show welcome screen
+    if (!activeAddressLoaded) {
+        return (
+            <div className="flex flex-col h-screen bg-dark-blue">
+                <Box
+                    style={{
+                        position: "absolute",
+                        top: "8px",
+                        right: "16px",
+                    }}
+                >
+                    <ConnectButton/>
+                </Box>
+            </div>
+        );
+    }
 
     return (
         <Router>
