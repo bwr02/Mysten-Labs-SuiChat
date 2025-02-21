3e315cd4a1c97c2920d740a693da0f157078d51b
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2025-02-20 17:16:46 -0800

    refactor and typescript fixes

--- frontend/src/components/ChatPanel.tsx
+++ frontend/src/components/ChatPanel.tsx
@@ -1,223 +1,20 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
-import { MessageInputField } from "./MessageInputField";
-import { getMessagesWithAddress, getDecryptedMessage, getSuiNSByAddress, getNameByAddress, editContact } from "../api/services/dbService";
-import { useSuiWallet } from "@/hooks/useSuiWallet";
-import { FaLink, FaInfoCircle } from 'react-icons/fa'; 
-import { formatTimestamp } from "@/api/services/messageService";
-import Modal from 'react-modal';
-
-
-interface Message {
-  sender: "sent" | "received";
-  text: string | null;
-  timestamp?: number;
-  txDigest?: string;
-}
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {MessageInputField} from "./MessageInputField";
+import {
+  getDecryptedMessage,
+  getMessagesWithAddress,
+  getNameByAddress,
+  getSuiNSByAddress
+} from "../api/services/dbService";
+import {useSuiWallet} from "@/hooks/useSuiWallet";
+import {formatTimestamp} from "@/api/services/messageService";
+import {RecipientBar} from "@/components/RecipientBar.tsx";
+import {MessageBubble} from "@/components/MessageBubble.tsx";
+import {Message, broadcastMessageParams} from "@/types/types.ts";
 
 interface ChatPanelProps {
   recipientAddress: string | null;
 }
-
-interface RecipientBarProps {
-  recipientName: string | null;
-  suins: string | null;
-  address: string | null;
-}
-
-
-const RecipientBar: React.FC<RecipientBarProps> = ({
-  recipientName,
-  suins,
-  address,
-}) => {
-  const [popupIsOpen, setPopupIsOpen] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(recipientName);
-
-  const popupRef = useRef<HTMLDivElement>(null);
-  const iconRef = useRef<HTMLDivElement>(null);
-
-  const togglePopup = () => {
-    // If closing the popup, also abort editing and reset input
-    if (popupIsOpen) {
-      setIsEditing(false);
-      setEditedName(null); // Reset the name if editing was in progress
-    }
-    setPopupIsOpen(!popupIsOpen);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        popupRef.current &&
-        !popupRef.current.contains(event.target as Node) &&
-        iconRef.current &&
-        !iconRef.current.contains(event.target as Node)
-      ) {
-        setPopupIsOpen(false);
-        setIsEditing(false); // Abort editing if clicked outside
-        setEditedName(null); // Reset to original name when closing
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [recipientName]);
-
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
-
-  const handleSaveClick = async () => {
-    await editContact(address, suins, editedName);
-    setIsEditing(false);
-  };
-
-  const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      await editContact(address, suins, editedName);
-      setIsEditing(false);
-    }
-  };
-
-  return (
-    <div className="w-full bg-light-blue text-gray-200 py-4 px-6 flex items-center justify-between shadow-md sticky top-0">
-      <h1 className="text-2xl font-bold flex items-center relative">
-        {recipientName && recipientName !== "null"
-          ? recipientName
-          : "No Contact Selected"}
-        {recipientName && recipientName !== "null" && (
-          <div ref={iconRef} className="relative inline-block">
-            <FaInfoCircle
-              className="ml-4 cursor-pointer text-lg"
-              onClick={togglePopup}
-            />
-            {popupIsOpen && (
-              <div
-                ref={popupRef}
-                className="absolute left-0 mt-5 bg-gray-600/90 text-white rounded-2xl shadow-lg w-64 p-4 z-10"
-              >
-                <div className="absolute -top-4 left-4 w-2 h-0 border-l-[10px] border-r-[10px] border-b-[16px] border-transparent border-b-gray-600/90"></div>
-
-                {isEditing ? (
-                  <>
-                    <input
-                      className="bg-gray-700 text-white rounded-lg p-1 mb-2 w-[80%] text-xs" 
-                      value={editedName}
-                      onChange={(e) => setEditedName(e.target.value)}
-                      onKeyPress={handleKeyPress}
-                      placeholder="Enter New Contact Name"
-                    />
-                    <button
-                      className="absolute top-2 right-2 text-xs text-gray-300 hover:text-white"
-                      onClick={handleSaveClick}
-                    >
-                      Save
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <h2 className="text-lg font-bold mb-2">{recipientName}</h2>
-                    <button
-                      className="absolute top-2 right-2 text-xs text-gray-300 hover:text-white"
-                      onClick={handleEditClick}
-                    >
-                      Edit
-                    </button>
-                  </>
-                )}
-
-                {suins && <p className="text-sm">SUINS: {suins}</p>}
-                {address && <p className="text-sm">Address: {address}</p>}
-              </div>
-            )}
-          </div>
-        )}
-      </h1>
-    </div>
-  );
-};
-
-
-
-
-
-
-const MessageBubble = React.memo(({ message, isLast }) => {
-  const [showTimestamp, setShowTimestamp] = useState(false);
-
-  const handleClick = () => {
-    setShowTimestamp(true);
-
-    // Hide the timestamp after 5 seconds for non-last messages
-    if (!isLast) {
-      const timer = setTimeout(() => {
-        setShowTimestamp(false);
-      }, 5000);
-
-      // Clear timeout if clicked again or component unmounts
-      return () => clearTimeout(timer);
-    }
-  };
-
-  return (
-    <div className="mb-2">
-      <div className={`flex items-center ${message.sender === 'sent' ? 'justify-end' : 'justify-start'}`}>
-        {/* Icon Link for Received Messages (on the left) */}
-        {message.txDigest && message.sender === 'received' && (
-          <a
-            href={`https://suiscan.xyz/testnet/tx/${message.txDigest}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline flex items-center mx-2"
-          >
-            <FaLink />
-          </a>
-        )}
-
-        {/* Message Bubble */}
-        <div
-          className={`p-3 rounded-2xl max-w-[50%] text-sm cursor-pointer ${
-            message.sender === 'sent'
-              ? 'bg-blue-700 text-white self-end'
-              : 'bg-gray-200 text-black self-start'
-          }`}
-          onClick={handleClick}
-        >
-          <span>{message.text}</span>
-        </div>
-
-        {/* Icon Link for Sent Messages (on the right) */}
-        {message.txDigest && message.sender === 'sent' && (
-          <a
-            href={`https://suiscan.xyz/testnet/tx/${message.txDigest}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline flex items-center mx-2"
-          >
-            <FaLink />
-          </a>
-        )}
-      </div>
-
-      {/* Timestamp aligned with the message bubble */}
-      {(isLast || showTimestamp) && (
-        <div className="flex mt-1">
-          {message.sender === 'received' ? (
-            <div className="text-xs text-gray-500 ml-[calc(2rem+8px)]"> {/* Aligned to start of the bubble */}
-              {message.timestamp}
-            </div>
-          ) : (
-            <div className="text-xs text-gray-500 ml-auto mr-[calc(2rem+8px)]"> {/* Aligned to end of the bubble */}
-              {message.timestamp}
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-});
 
 export const ChatPanel: React.FC<ChatPanelProps> = ({ recipientAddress }) => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -284,7 +81,7 @@ export const ChatPanel: React.FC<ChatPanelProps> = ({ recipientAddress }) => {
   useEffect(() => {
       const ws = new WebSocket('ws://localhost:8080');
 
-      const handleNewMessage = async (messageData: any) => {
+      const handleNewMessage = async (messageData: broadcastMessageParams) => {
         try {
             const decryptedMessage = await getDecryptedMessage(
                 recipientAddress,
@@ -317,7 +114,7 @@ export const ChatPanel: React.FC<ChatPanelProps> = ({ recipientAddress }) => {
             // console.log("New message received:", data);
             handleNewMessage(data.message);
           }
-        };
+        }
       };
       return () => ws.close(); // Cleanup on unmount
   }, [recipientAddress, wallet]);
