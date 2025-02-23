fee50649e02ff278c9926b1bdae5bda356c04d93
Chloe Ta <cqta@ucsb.edu>
2025-02-22 16:42:06 -0800

    added public_keys contract and updated packageId to reflect change

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -2,7 +2,7 @@ import { env } from '../config/env';
 
 export const CONFIG = {
     MESSAGE_CONTRACT: {
-        packageId: '0x6f06ff51a46a11e1eacb086822eb42405a1c4636971a4d8b8eccdf55c3b39a9d',
+        packageId: '0x93f37be3ee764a8ee7b929b9c418c50ca7affa67dea06468b9a857fefed15a8b',
     },
     NETWORK_URL: env.NETWORK_URL
 };
